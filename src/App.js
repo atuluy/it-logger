a4a439aada9b945f31cf4a2a736e915fac4619e3
ANIL TULUY <atuluy74@gmail.com>
2022-09-26 13:29:58 +0300

    Import Materialize-css/js and add Material Icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+
 const App = () => {
+  useEffect(() => {
+    // Initialize Materialize JavaScript in order to use Materialize Modals etc...
+    M.AutoInit();
+  });
+
   return <div className="App">Hello Anil</div>;
 };
 
