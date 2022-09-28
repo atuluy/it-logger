50034a07c90a0ef43688cef9054f6a3f56f6df8a
ANIL TULUY <atuluy74@gmail.com>
2022-09-28 19:31:46 +0300

    Add EditLogModal component

---
+++ src/components/logs/EditLogModal.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const EditLogModal = () => {
+  const [message, setMessage] = useState("");
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Please enter a message and a tech" });
+    } else {
+      console.log(message, tech, attention);
+
+      // Clear fields
+      setMessage("");
+      setTech("");
+      setAttention(false);
+    }
+  };
+
+  return (
+    // The div id must match with the modal-trigger buttons href
+    <div id="edit-log-modal" className="modal" style={modalStyle}>
+      <div className="modal-content">
+        <h4>Enter System Log</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <label htmlFor="message" className="active">
+              Log Message
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <select
+              className="browser-default"
+              name="tech"
+              value={tech}
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technician
+              </option>
+              <option value="Test1">Test1</option>
+              <option value="Test2">Test2</option>
+              <option value="Test3">Test3</option>
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  className="filled-in"
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          className="modal-close waves-effect blue waves-light btn"
+          onClick={onSubmit}
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%",
+};
+
+export default EditLogModal;
