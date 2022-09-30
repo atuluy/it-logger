85833e651d235e2548725bacdb1acd75f0f816e9
ANIL TULUY <atuluy74@gmail.com>
2022-10-01 00:53:52 +0300

    Create logActions and add to logReducer

---
+++ src/actions/logActions.js
@@ -0,0 +1,43 @@
+import { GET_LOGS, LOGS_ERROR, SET_LOADING } from "../types/types";
+
+// Making an asynchronous call to our backend we will use redux-thunk Version 1
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+
+//     const res = await fetch("/logs");
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+// Making an asynchronous call to our backend we will use redux-thunk Version 2
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
