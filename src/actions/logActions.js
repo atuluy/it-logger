c2db481b2e5b9b90c4ec8b413c753dc015e1f834
ANIL TULUY <atuluy74@gmail.com>
2022-10-01 17:40:29 +0300

    Add ADD_LOG action to logActions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, LOGS_ERROR, SET_LOADING } from "../types/types";
+import { GET_LOGS, LOGS_ERROR, SET_LOADING, ADD_LOG } from "../types/types";
 
 // Making an asynchronous call to our backend we will use redux-thunk / code Version 1
 // export const getLogs = () => {
@@ -25,6 +25,32 @@ export const getLogs = () => async (dispatch) => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// Add new log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data,
     });
   } catch (error) {
