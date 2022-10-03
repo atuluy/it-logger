791bbe05ab26be67583352baf3012d91db2011d1
ANIL TULUY <atuluy74@gmail.com>
2022-10-03 12:30:31 +0300

    Add techReducer into index.js to combine it with reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import logReducer from "./logReducer";
+import techReducer from "./techReducer";
 
 export default combineReducers({
   log: logReducer, // State is called log
+  tech: techReducer, // State is called tech
 });
