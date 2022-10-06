9449f2c6dc3bcef12e249224ea531d2a65697d3b
ANIL TULUY <atuluy74@gmail.com>
2022-10-06 11:39:46 +0300

    Implement addTech function into AddTechModal

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddTechModal = () => {
+// Actions
+import { addTech } from "../../actions/techActions";
+
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
@@ -9,7 +14,14 @@ const AddTechModal = () => {
     if (firstName === "" || lastName === "") {
       M.toast({ html: "Please enter the first name and the last name" });
     } else {
-      console.log(firstName, lastName);
+      addTech({
+        firstName,
+        lastName,
+      });
+
+      M.toast({
+        html: `${firstName} ${lastName} wad added as a new technician.`,
+      });
 
       // Clear fields
       setFirstName("");
@@ -62,4 +74,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addTech })(AddTechModal);
