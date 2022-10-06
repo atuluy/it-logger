9236c25480602785d8430b9bc6316b48a91e97cb
ANIL TULUY <atuluy74@gmail.com>
2022-10-06 12:11:02 +0300

    Implement DELETE_TECH action

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const TechItem = ({ tech }) => {
+import M from "materialize-css/dist/js/materialize.min.js";
+
+// Actions
+import { deleteTech } from "../../actions/techActions";
+
+const TechItem = ({ tech, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(tech.id);
+    M.toast({
+      html: `${tech.firstName} ${tech.lastName} has been removed from the list`,
+    });
+  };
+
   return (
     <li className="collection-item">
       <div>
         {tech.firstName} {tech.lastName}
-        <a href="#!" className="secondary-content">
+        <a href="#!" className="secondary-content" onClick={onDelete}>
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -16,6 +29,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
   tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
